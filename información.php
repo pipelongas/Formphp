2afea9949dc74a59dc8107f1ff04a2db0bb4a2dd
pipelongas <d.perezcan@colmilap.edu.co>
2024-07-30 16:30:50 -0500

    Update información.php

--- información.php
+++ información.php
@@ -2,3 +2,6 @@
 $Identificacion= $_POST ['Identificacion' ];
 $NomApellidos=$_POST ['NomApellidos'];
 $Cargo=$_POST ['Cargo'];
+$Salario=$_POST ['Salario'];
+echo "La identificacion del Empleado es: ".Identificacion;
+echo
